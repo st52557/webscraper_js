4b5a9b2e2c8a4429aead53973beadae6c41024c7
Lukas Semorad <st52557@student.upce.cz>
2022-06-29 15:54:41 +0200

    tweaks

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-Q
-!nodejs-webscraper-main/src/app.js,4\1\4152d7884a5f7bf7d9dd409670d05e17bc5e294a
--- nodejs-webscraper-main/src/index.js
+++ nodejs-webscraper-main/src/index.js
@@ -5,26 +5,60 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
+const rateLimit = require('express-rate-limit')
 
 var fs = require('fs');
+var bodyParser = require('body-parser');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', "./nodejs-webscraper-main" + '/views');
+app.use("/public", express.static('public'));
 
+const apiRequestLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 10, // limit each IP to 10 requests per windowMs
+    handler: function (req, res, /*next*/) {
+        return res.status(429).json({
+            error: 'You sent too many requests. Please wait a while then try again'
+        })
+    }
+})
+
+// Use the limit rule as an application middleware
+app.use(apiRequestLimiter)
 
 let urlNavrat = 'https://www.navratdoreality.cz/'
 let urlNavratNumberOfPages = 3;
 let minRating = 7;
-var mapNavrat = new Map();
+let loadImages = 1;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+var jsonParser = bodyParser.json()
+
+app.post('/auth', jsonParser, function (req, res) {
+    console.log(req.body);
+    if(req.body){
+        console.log(req.body.pass);
+    }
+    res.setHeader('Content-type', 'text/html');
+    res.render("response-index.html", );
+})
+
 
 
 app.get('/', function (req, res) {
+    console.log("get / : "+req.body);
+    if(req.body){
+        console.log(req.body.pass);
+    }
     res.setHeader('Content-type', 'text/html');
     res.render("response-index.html", );
 
 })
 
+
 app.get('/dira', function (req, res) {
+
     res.setHeader('Content-type', 'text/html');
     res.render("response-dira.html", );
 
@@ -32,12 +66,18 @@ app.get('/dira', function (req, res) {
 
 
 app.get('/navrat', async (req, res) => {
-    mapNavrat.clear();
+    var mapNavrat = new Map();
     urlNavratNumberOfPages = 3;
     minRating = 7;
+    loadImages = 1;
     if(req.query.load){
         if(parseInt(req.query.load) >= 1 && parseInt(req.query.load) < 21)
         urlNavratNumberOfPages = parseInt(req.query.load);
+    }
+
+    if(req.query.img){
+        if(parseInt(req.query.img) === 0 || parseInt(req.query.img) === 1)
+            loadImages = parseInt(req.query.img);
     }
 
     if(req.query.rating){
@@ -66,7 +106,10 @@ app.get('/navrat', async (req, res) => {
                 $('.mainbar .item', html).each(function () { //<-- cannot be a function expression
                     const title = $(this).find('a strong').text()
                     const url = $(this).find('a').attr('href')
-                    const img = $(this).find('img').attr('src')
+                    let img = "images off ";
+                    if(loadImages){
+                        img = $(this).find('img').attr('src')
+                    }
                     const rating = $(this).find('.item-info-rank-val').text()
                     const date = $(this).find('.item-meta').text()
                     const info = $(this).find('.p-like').text()
